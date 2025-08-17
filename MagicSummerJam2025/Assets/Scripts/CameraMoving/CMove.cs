aa76c634dac7a336ac78c5160ae5416d24711a85
VIktoriya Cherevatenko <cherevatenkoviktoriya@gmail.com>
2025-08-17 21:52:17 +0300

    Scripts fixes

---
+++ MagicSummerJam2025/Assets/Scripts/CameraMoving/CMove.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CMove : MonoBehaviour
+{
+    public Camera MainCamera;
+    void Awake()
+    {
+        MainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        
+    }
+}
