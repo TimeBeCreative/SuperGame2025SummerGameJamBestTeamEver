c651e8c2bc2805ce484826e18603da951109b0fa
VIktoriya Cherevatenko <cherevatenkoviktoriya@gmail.com>
2025-08-19 14:48:52 +0300

    Boat script jump added

--- MagicSummerJam2025/Assets/Scripts/Boat.cs
+++ MagicSummerJam2025/Assets/Scripts/Boat.cs
@@ -5,13 +5,20 @@ using Unity.Collections;
 public class Boat : MonoBehaviour
 {
 
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     
     private void FixedUpdate()
     {
        
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(Vector3.forward * 5f *  Time.deltaTime);
+            transform.Translate(Vector3.forward * 5f *  Time.deltaTime, Space.Self);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
@@ -23,7 +30,24 @@ public class Boat : MonoBehaviour
         {
             transform.Rotate(Vector3.up * -100f * Time.deltaTime);
         }
-       
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _animator.SetBool("IsJumping", true);
+        }
+        else
+        {
+            _animator.SetBool("IsJumping", false);
+
+            Vector3 pos = transform.position;
+            pos.y = Mathf.Lerp(pos.y, -0.45f, Time.deltaTime * 2f);
+            transform.position = pos;
+
+            Quaternion targetRotation = Quaternion.Euler(0f, transform.eulerAngles.y, 0f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2f);
+
+        }
+     
 
 
      
